26770bf9bd6e795de01dee78acdb80d9b6fa8293
VI-Alumno <VI-Alumno@duoc.acad>
2026-05-22 11:21:47 -0400

    fix: pass de db

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 const dbConnection = mysql.createConnection({
     host: process.env.DB_HOST || 'db',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
+    password: process.env.DB_PASSWORD || 'Innovatech2025!',
     database: process.env.DB_NAME || 'proyecto_db',
     port: process.env.DB_PORT || 3306
 });
