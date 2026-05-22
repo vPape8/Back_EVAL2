b2adc94d6f87006393de40a18a32286ba29a7847
Martin Coronel <85178396+vPape8@users.noreply.github.com>
2026-05-22 10:52:45 -0400

    Change database host from 'localhost' to 'db'

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 // Configuración de la conexión a la base de datos MySQL
 const dbConnection = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || 'db',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'proyecto_db',
